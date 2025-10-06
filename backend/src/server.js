f0da7c2e2609743c2fd6fa81dc67f0730bcbacd0
hyooeewee <hughie-ngai@outlook.com>
2025-10-06 17:38:33 +0800

    🚀 Release: v0.0.1

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 
+import path from 'path';
 import { connectDB } from './config/db.js';
 import rateLimiter from './middlewares/rateLimiter.js';
 import notesRouter from './routers/notesRouter.js';
@@ -13,13 +14,26 @@ const PORT = process.env.PORT || 5001;
 
 const app = express();
 
+// development
+if (process.env.NODE_ENV !== 'production') {
+	app.use(cors({ origin: 'http://localhost:5173' })); // for CORS, must be top
+}
+
 // middlewares
-app.use(cors({ origin: 'http://localhost:5173' })); // for CORS, must be top
 app.use(express.json()); // for parsing application/json: req.body
 app.use(rateLimiter); // for rate limiting
 
 // routers
 app.use('/api/notes', notesRouter);
+
+// deployment
+if (process.env.NODE_ENV === 'production') {
+	const __dirname = path.resolve();
+	app.use(express.static(path.join(__dirname, '../frontend', 'dist')));
+	app.get('*', (_, res) => {
+		res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+	});
+}
 
 connectDB().then(() => {
 	app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}!`));
